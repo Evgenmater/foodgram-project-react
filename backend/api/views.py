8005c066755287ddd4384d686264056fd158ba74
Евгений Хлебнев <hlebnev@yandex.ru>
2023-09-22 23:36:59 +0300

    fix5

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,7 +6,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+from rest_framework.pagination import (
+    PageNumberPagination, LimitOffsetPagination
+)
 from rest_framework.permissions import (
     AllowAny, IsAuthenticatedOrReadOnly, IsAuthenticated
 )
