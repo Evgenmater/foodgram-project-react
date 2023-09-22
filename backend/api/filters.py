ab97f3841527783df5d98a2287e1ac31c305ae51
Евгений Хлебнев <hlebnev@yandex.ru>
2023-09-22 17:37:04 +0300

    fi2

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -35,12 +35,12 @@ class RecipeFilter(FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if value:
-            return queryset.filter(recipes_favorited__user=self.request.user)
+            return queryset.filter(user_favorited__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value:
             return queryset.filter(
-                recipe_shopping_cart__user=self.request.user
+                user_shopping_cart__user=self.request.user
             )
         return queryset
