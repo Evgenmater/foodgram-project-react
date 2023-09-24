8bd2aba3f186f14c1f57ad778421abac8ade169d
Евгений Хлебнев <hlebnev@yandex.ru>
2023-09-24 18:33:06 +0300

    fix errors3

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,23 +1,23 @@
-from django.urls import include, path
-from djoser.views import TokenCreateView, TokenDestroyView
-from rest_framework import routers
+# from django.urls import include, path
+# from djoser.views import TokenCreateView, TokenDestroyView
+# from rest_framework import routers
 
-from api.views import TagViewSet, RecipeViewSet, UserViewSet, IngredientViewSet
+# from api.views import TagViewSet, RecipeViewSet, UserViewSet, IngredientViewSet
 
-app_name = 'api'
+# app_name = 'api'
 
-router_v1 = routers.DefaultRouter()
+# router_v1 = routers.DefaultRouter()
 
-router_v1.register(r'tags', TagViewSet, basename='tags')
-router_v1.register(r'users', UserViewSet, basename='users')
-router_v1.register(r'recipes', RecipeViewSet, basename='recipes')
-router_v1.register(r'ingredients', IngredientViewSet, basename='ingredients')
+# router_v1.register(r'tags', TagViewSet, basename='tags')
+# router_v1.register(r'users', UserViewSet, basename='users')
+# router_v1.register(r'recipes', RecipeViewSet, basename='recipes')
+# router_v1.register(r'ingredients', IngredientViewSet, basename='ingredients')
 
 
-urlpatterns = [
-    path('', include(router_v1.urls)),
-    path('', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
-    path('token/login/', TokenCreateView.as_view(), name='login'),
-    path('token/logout/', TokenDestroyView.as_view(), name='logout'),
-]
+# urlpatterns = [
+#     path('', include(router_v1.urls)),
+#     path('', include('djoser.urls')),
+#     path('auth/', include('djoser.urls.authtoken')),
+#     path('token/login/', TokenCreateView.as_view(), name='login'),
+#     path('token/logout/', TokenDestroyView.as_view(), name='logout'),
+# ]
