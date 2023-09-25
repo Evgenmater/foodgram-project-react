4c96ecd5b4f234a61906a478f22b8fb1ab04ebe3
Евгений Хлебнев <hlebnev@yandex.ru>
2023-09-25 19:14:30 +0300

    fixshmix

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,4 @@
 from django.db.models import Q
-# from django_filters import filters, FilterSet
 from django_filters.rest_framework import filters, FilterSet
 from recipes.models import Ingredient, Recipe
 
@@ -41,45 +40,3 @@ class RecipeFilter(FilterSet):
         if value:
             return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
-
-
-# class IngredientFilter(FilterSet):
-#     """Поиск игредиента по названию."""
-
-#     name = filters.CharFilter(
-#         field_name='name', method='get_name_or_contains'
-#     )
-
-#     def get_name_or_contains(self, queryset, name, value):
-#         return queryset.filter(
-#             Q(name__istartswith=value) | Q(name__icontains=value)
-#         ).distinct()
-
-#     class Meta:
-#         model = Ingredient
-#         fields = ('name',)
-
-
-# class RecipeFilter(FilterSet):
-#     """Фильтрация рецептов."""
-
-#     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-#     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
-#     is_in_shopping_cart = filters.BooleanFilter(
-#         method='filter_is_in_shopping_cart')
-
-#     class Meta:
-#         model = Recipe
-#         fields = ('tags', 'author')
-
-#     def filter_is_favorited(self, queryset, name, value):
-#         if value:
-#             return queryset.filter(favorited__user=self.request.user)
-#         return queryset
-
-#     def filter_is_in_shopping_cart(self, queryset, name, value):
-#         if value:
-#             return queryset.filter(
-#                 shopping_cart__user=self.request.user
-#             )
-#         return queryset
